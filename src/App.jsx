c8240c216c02bf2170bf7880a5ad4d556b5868aa
José Alejandro Puente Velásquez <jpuentevelasquez@gmail.com>
2025-10-24 15:12:19 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from './pages/home/Home'
+
+function App() {
+  return (
+    <div style={{
+      margin: 0,
+      minHeight: '100%'
+    }}>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,88 @@
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <Box sx={{
+            paddingX: 10,
+            alignItems: 'center',
+            backgroundColor: 'white',
+            margin: 0
+        }}>
+            <AppBar
+                color='transparent'
+                sx={{
+                    boxShadow: 'none'
+                }}
+                position='sticky'
+            >
+                <Toolbar
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between'
+                    }}
+                >
+                    <IconButton
+                        sx={{
+                            ":hover": {
+                                backgroundColor: 'transparent'
+                            }
+                        }}
+                    >
+                        <img src="./images/logo.png" alt="logo" height='50' />
+                    </IconButton>
+                    <Box>
+                        <Button
+                            variant='outlined'
+                            sx={{
+                                border: 0,
+                                color: 'black',
+                                textTransform: "none",
+                                
+                                ":hover": {
+                                    backgroundColor: 'transparent'
+                                }
+                            }}
+                        >
+                            about
+                        </Button>
+                        <Button
+                            variant='outlined'
+                            sx={{
+                                border: 0,
+                                color: 'black',
+                                textTransform: "none",
+                                
+                                ":hover": {
+                                    backgroundColor: 'transparent'
+                                }
+                            }}
+                        >
+                            portfolio
+                        </Button>
+                        <Button
+                            variant='outlined'
+                            sx={{
+                                border: 0,
+                                color: 'black',
+                                textTransform: "none",
+                                
+                                ":hover": {
+                                    backgroundColor: 'transparent'
+                                }
+                            }}
+                        >
+                            contact
+                        </Button>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+
+            <Box>
+                
+            </Box>
+        </Box>
+    )
+}
+
+export default Home
